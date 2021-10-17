4efd11054a5e754d8dd8705b6c0110e84e96dd43
Jophin123 <jophinfrancis1997@gmail.com>
2021-10-17 13:04:19 +0530

    commited

--- script/script.js
+++ script/script.js
@@ -59,3 +59,18 @@
 
  	$('.slider-single').slick('slickGoTo', goToSingleSlide);
  });
+
+ 
+ 
+
+ var counter = 0;
+
+function subtract(){
+  counter = counter-1;
+  document.getElementById("counter").innerHTML = counter;
+}
+
+function add(){
+  counter = counter + 1;
+  document.getElementById("counter").innerHTML = counter;
+}
